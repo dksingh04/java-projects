e61e5f30e220d3840f83668be08919979afe460f
Deepak Singh <deepaksingh04@gmail.com>
2021-05-27 23:38:29 +0530

    some more coding problems

--- DataStructureAndAlgorithm/src/com/dk/codeproblems/RotateFromNthNode.java
+++ DataStructureAndAlgorithm/src/com/dk/codeproblems/RotateFromNthNode.java
@@ -1,4 +1,67 @@
 package com.dk.codeproblems;
 
 public class RotateFromNthNode {
+
+    public static void main(String ...args){
+            int[] list1 = {1, 2, 3, 4, 5};
+            LinkedListNode listHead = LinkedList.createLinkedList(list1);
+
+            System.out.print("Original list: ");
+            LinkedList.display(listHead);
+
+            listHead = rotateList(listHead, 2);
+            System.out.print("List rotated by 2: ");
+            LinkedList.display(listHead);
+    }
+
+    private static LinkedListNode rotateList(LinkedListNode head, int n){
+
+        if(head == null || n == 0){
+            return head;
+        }
+
+        int len = len(head);
+
+        n = adjustNthNode(n, len);
+
+        //TODO rotate the list here
+        int rotationPos = len - n - 1;
+        LinkedListNode rotationNodeP = head;
+        while(rotationPos > 0){
+            rotationNodeP = rotationNodeP.next;
+            --rotationPos;
+        }
+
+        LinkedListNode temp = rotationNodeP.next;
+        LinkedListNode tempCurr = temp;
+        while(tempCurr.next != null){
+            tempCurr = tempCurr.next;
+        }
+
+        tempCurr.next = head;
+        rotationNodeP.next = null;
+        head = temp;
+        //LinkedList.display(temp);
+
+        return head;
+    }
+
+    private static int len(LinkedListNode head){
+        int len = 0;
+        LinkedListNode curr = head;
+        while(curr != null){
+            curr = curr.next;
+            len++;
+        }
+
+        return len;
+    }
+
+    private static int adjustNthNode(int n, int len){
+        n = n % len;
+        if(n < 0){
+            n = n + len;
+        }
+        return n;
+    }
 }
