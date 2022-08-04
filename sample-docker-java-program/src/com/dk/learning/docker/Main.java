a6d0bef3cecd7640b328d00bc2a9b33874539768
Deepak Singh <deepaksingh04@gmail.com>
2022-08-04 13:54:42 +0530

    Sample Java program to run in docker container.

---
+++ sample-docker-java-program/src/com/dk/learning/docker/Main.java
@@ -0,0 +1,8 @@
+package com.dk.learning.docker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Running Java program in Docker container!");
+    }
+}
